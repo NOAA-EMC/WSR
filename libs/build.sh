9f811600e04fcca88ae19e3f9240c0dd31e9a253
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2020-11-13 11:12:58 -0500

    Improve libs/build.sh
    1) Improve the workflow of libs/build.sh
    2) Update the README based on the item 1)
    
     On branch feature/add_rocoto
    	modified:   README
    	modified:   libs/build.sh
    
    Refs: #4

--- libs/build.sh
+++ libs/build.sh
@@ -2,10 +2,23 @@
 #
 # build the ECMWF library for WSR
 #
-libdir=`dirname $0`
-if [[ $libdir = '.' ]]; then
-  libdir=`pwd`
+
+set -eux
+
+#libdir=`dirname $0`
+#if [[ $libdir = '.' ]]; then
+libdir=`pwd`
+#fi
+
+if [ -d $libdir/grib_api-1.9.16-working ]; then
+  echo "Deleting $libdir/grib_api-1.9.16-working"
+  rm -rf $libdir/grib_api-1.9.16-working
 fi
+if [ -d $libdir/ecmwf_grib_api-1.9.16 ]; then
+  echo "Deleting $libdir/ecmwf_grib_api-1.9.16"
+  rm -rf $libdir/ecmwf_grib_api-1.9.16
+fi
+
 cp -pr $libdir/grib_api-1.9.16 $libdir/grib_api-1.9.16-working
 cd $libdir/grib_api-1.9.16-working
 rc=$?
@@ -22,11 +35,20 @@ if (( rc == 0 )); then
   else
     echo to continue:
     echo cd grib_api-1.9.16-working
-    echo make
-    echo make check
-    echo make install
+    echo "----------------------------- make ..."
+    make
+    echo "----------------------------- make check ..."
+    make check
+    echo "----------------------------- make install..."
+    make install
   fi
 else
   echo cd $libdir/grib_api-1.9.16-working FAILED rc=$rc
+  echo $rc
 fi
+
+echo;echo " .... Build grib_api-1.9.16 finished .... "
+echo;
+
+exit 0
 
