16bfb4183f3773eb217d9cb23b12e404d5278783
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2021-03-25 15:26:36 -0400

    Improve sorc/build.sh for both dell and acorn
     On branch feature/port2wcoss2
    	modified:   build.sh
    
    Refs: #6

--- sorc/build.sh
+++ sorc/build.sh
@@ -6,6 +6,30 @@ set -eux
 
 hostname
 
+while getopts m: option
+do
+    case "${option}"
+    in
+        m) machine=${OPTARG};;
+    esac
+done
+
+machine=${machine:-acorn} #dell, acorn
+
+if [ $machine = "dell" ]; then
+  module purge
+  source /usrx/local/prod/lmod/lmod/init/bash
+  module load ips/19.0.5.281
+
+  module list
+elif [ $machine = "acorn" ]; then
+  source /apps/prod/lmodules/startLmod
+  module load intel/19.1.3.304
+
+  module list
+fi
+
+
 dirpwd=`pwd`
 execlist="calcperts calcspread circlevr dtsset flights_allnorms rawin_allnorms reformat"
 execlist="$execlist sig_pac sigvar_allnorms summ_allnorms tcoeffuvt tgr_special xvvest_allnorms"
@@ -26,7 +50,7 @@ do
   cd $sorcdir
   rc=$?
   if (( rc == 0 )); then
-    make -f makefile.dell $command
+    make -f makefile.$machine $command
     rc=$?
     echo in $sorcdir
     if (( rc == 0 )); then
@@ -34,7 +58,7 @@ do
       make clean -f makefile.dell
       (( ns = ns + 1 ))
     else
-      echo make -f makefile.dell FAILED rc=$?
+      echo make -f makefile.$machine FAILED rc=$?
       (( nf = nf + 1 ))
       failstring="$failstring $exec"
     fi
