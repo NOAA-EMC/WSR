5ade226451fb2174b088331b231058fbc6592ac0
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2021-04-28 16:48:01 -0400

    Update the modules in bin files and versions
    1) Update the modules in bin files for wsr_main and wsr_prep
    2) Update the version in versions/wsr_acorn.ver
    3) Add KEEPDATA to user_full.conf
    4) Modify setbase to identify dell and add to identify acorn
    
     On branch feature/port2wcoss2
     Changes to be committed:
    	modified:   rocoto/bin/acorn/wsr_main.sh
    	modified:   rocoto/bin/acorn/wsr_prep.sh
    	modified:   rocoto/parm/setbase
    	modified:   rocoto/user_full.conf
    	modified:   versions/wsr_acorn.ver
    
    Refs: #6

--- rocoto/bin/acorn/wsr_main.sh
+++ rocoto/bin/acorn/wsr_main.sh
@@ -5,7 +5,7 @@ ulimit -s unlimited
 ulimit -a
 
 # module_ver.h
-. $SOURCEDIR/versions/wsr_wcoss_dell_p3.ver
+. $SOURCEDIR/versions/wsr_acorn.ver
 
 # Load modules
 module list
@@ -13,22 +13,27 @@ module purge
 
 source /apps/prod/lmodules/startLmod
 module load envvar/$envvar_ver
-module load intel/$intel_ver
+module load intel/$intel_ver PrgEnv-intel
 module load intel/$intel_ver/cray-mpich/$mpich_ver #impi/$impi_ver
+module load cray-pals/1.0.8
+
 module load prod_util/$prod_util_ver
 module load prod_envir/$prod_envir_ver
-
 module load grib_util/$grib_util_ver
 
-#module load lsf/$lsf_ver
+module load cce/11.0.2
+module load gcc/10.2.0
+module load libjpeg/9c
 
-module load cfp/$cfp_ver
-#export USE_CFP=YES
+module load lsf/$lsf_ver
+
+module load CFP/$CFP_ver
+export USE_CFP=YES
 
 module list
 
 # For Development
-. $GEFS_ROCOTO/bin/wcoss_dell_p3/common.sh
+. $GEFS_ROCOTO/bin/acorn/common.sh
 
 # Export List
 
--- rocoto/bin/acorn/wsr_prep.sh
+++ rocoto/bin/acorn/wsr_prep.sh
@@ -16,10 +16,16 @@ source /apps/prod/lmodules/startLmod
 module load envvar/$envvar_ver
 module load intel/$intel_ver PrgEnv-intel
 module load intel/$intel_ver/cray-mpich/$mpich_ver #impi/$impi_ver
+module load cray-pals/1.0.8
+
 module load prod_util/$prod_util_ver
 module load prod_envir/$prod_envir_ver
 
 module load grib_util/$grib_util_ver
+
+module load cce/11.0.2
+module load gcc/10.2.0
+module load libjpeg/9c
 
 module load lsf/$lsf_ver
 
