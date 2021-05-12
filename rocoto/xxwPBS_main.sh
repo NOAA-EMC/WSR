5131cde3807845f6952832081bd85d4ac28817e6
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2021-05-12 14:13:11 -0400

    Improve xxwPBS_main.sh
     On branch feature/port2wcoss2
    	modified:   rocoto/xxwPBS_main.sh
    
    Refs: #6

--- rocoto/xxwPBS_main.sh
+++ rocoto/xxwPBS_main.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#PBS -N wsr_20201120_ws_in
+#PBS -N wsr_main_20201120
 ##PBS -o /lfs/h1/emc/ptmp/Xianwu.Xue/o/wsr_port2wcoss2/com/output/dev/20201120/wsr_main_00.%J
 #PBS -j oe
 #PBS -l select=1:ncpus=48
@@ -17,20 +17,20 @@ module purge
 export envir='dev'
 export RUN_ENVIR='dev'
 export WHERE_AM_I='acorn'
-export GEFS_ROCOTO='/lfs/h1/emc/ens/noscrub/Xianwu.Xue/wsr/wsr_port2wcoss2/rocoto'
-export WORKDIR='/lfs/h1/emc/ptmp/Xianwu.Xue/o/wsr_port2wcoss2'
-export EXPID='wsr_port2wcoss2'
+export GEFS_ROCOTO='/lfs/h1/emc/ens/noscrub/Xianwu.Xue/wsr/port2wcoss2/rocoto'
+export WORKDIR='/lfs/h1/emc/ptmp/Xianwu.Xue/o/port2wcoss2'
+export EXPID='port2wcoss2'
 export gefs_cych='24'
 export PDY='20201120'
 export cyc='00'
 export cyc_fcst='00'
-export SOURCEDIR='/lfs/h1/emc/ens/noscrub/Xianwu.Xue/wsr/wsr_port2wcoss2'
+export SOURCEDIR='/lfs/h1/emc/ens/noscrub/Xianwu.Xue/wsr/port2wcoss2'
 export job=wsr_main_${EXPID}_${PDY}${cyc}
 
 
 #bsub -J wsr_dell_all_2020112000_ws_in -P GEN-T2O -o /gpfs/dell2/ptmp/Xianwu.Xue/o/wsr_dell_all/com/output/dev/20201120/wsr_main_00.%J -W 0:45 -q dev -R span[ptile=16] -n 48 /gpfs/dell3/usrx/local/dev/emc_rocoto/complete/sbin/lsfwrapper.sh /gpfs/dell2/emc/retros/noscrub/Xianwu.Xue/For_WSR/wsr_dell_all/rocoto/bin/wcoss_dell_p3/wsr_main.sh}}
 
-/lfs/h1/emc/ens/noscrub/Xianwu.Xue/wsr/wsr_port2wcoss2/rocoto/bin/acorn/wsr_main.sh
+/lfs/h1/emc/ens/noscrub/Xianwu.Xue/wsr/port2wcoss2/rocoto/bin/acorn/wsr_main.sh
 
 ~                                                                                                                                                              
 
