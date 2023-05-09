53ee819b5f9168c67f2b30c856bf70167b09263f
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2023-05-09 18:51:26 -0400

    Port pyWSR to WCOSS2
     On branch feature/port_pyWSR_to_WCOSS2
    	modified:   rocoto/bin/wcoss2/common.sh
    	modified:   rocoto/bin/wcoss2/wsr_main.sh
    	modified:   rocoto/bin/wcoss2/wsr_prep.sh
    	modified:   rocoto/py/GEFS_Crontab.py
    	modified:   rocoto/py/GEFS_Parm.py
    	modified:   rocoto/py/GEFS_UserConfig.py
    	modified:   rocoto/py/GEFS_XML.py
    	modified:   rocoto/py/GEFS_XML_For_Tasks.py
    	modified:   rocoto/py/add_crontab.py
    	modified:   rocoto/py/job_id.conf
    	modified:   rocoto/py/run_pyGEFS.py
    	modified:   rocoto/py/user_wcoss2.conf
    	modified:   rocoto/user_full.conf
    
    Refs: #15

--- rocoto/bin/wcoss2/wsr_prep.sh
+++ rocoto/bin/wcoss2/wsr_prep.sh
@@ -1,34 +1,24 @@
-#!/bin/ksh
+#! /usr/bin/env bash
 
 set -x
 ulimit -s unlimited
 ulimit -a
 
 # module_ver.h
-. $SOURCEDIR/versions/wsr_wcoss2.ver
+. $SOURCEDIR/versions/run.ver
 
 # Load modules
-#module list
+module list
 module purge
 
 module load envvar/$envvar_ver
-module load intel/$intel_ver PrgEnv-intel
-
+module load intel/$intel_ver
+module load PrgEnv-intel/$PrgEnv_intel_ver
 module load craype/$craype_ver
 module load cray-mpich/$cray_mpich_ver
 module load cray-pals/$cray_pals_ver
-
-module load prod_util/$prod_util_ver
-module load prod_envir/$prod_envir_ver
-
-module load grib_util/$grib_util_ver
-
-#module load cce/11.0.2
-#module load gcc/10.2.0
 module load libjpeg/$libjpeg_ver
-
-#module load lsf/$lsf_ver
-
+module load grib_util/$grib_util_ver
 module load cfp/$cfp_ver
 export USE_CFP=YES
 
@@ -38,12 +28,16 @@ module list
 . $GEFS_ROCOTO/bin/wcoss2/common.sh
 
 # Export List
+set -x
+
 export MP_EUIDEVICE=sn_all
 export MP_EUILIB=us
 export MP_TASK_AFFINITY=core
 
 export MP_PGMMODEL=mpmd
 export MP_CSS_INTERRUPT=yes
+
+export envir=prod
 
 # CALL executable job script here
 $SOURCEDIR/jobs/JWSR_PREP
